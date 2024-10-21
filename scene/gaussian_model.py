Fix 2d gradient scale

scene/gaussian_model.py
@@ -513,10 +513,12 @@ class GaussianModel:
 
         torch.cuda.empty_cache()
 
-    def add_densification_stats(self, viewspace_point_tensor, update_filter, gaussian_ids=None):
-        if gaussian_ids is not None:
+    def add_densification_stats(self, viewspace_point_tensor, update_filter, meta=None):
+        if meta is not None:
             means2d_grad = torch.zeros(self.get_xyz.shape[0], 2, device="cuda")
-            means2d_grad[gaussian_ids] = viewspace_point_tensor.grad
+            means2d_grad[meta["gaussian_ids"]] = viewspace_point_tensor.grad
+            means2d_grad[..., 0] *= meta["width"] / 2.0 * meta["n_cameras"]
+            means2d_grad[..., 1] *= meta["height"] / 2.0 * meta["n_cameras"]
             self.xyz_gradient_accum[update_filter] += torch.norm(means2d_grad[update_filter,:2], dim=-1, keepdim=True)
         else:
             self.xyz_gradient_accum[update_filter] += torch.norm(viewspace_point_tensor.grad[update_filter,:2], dim=-1, keepdim=True)

train_goliath.py
@@ -126,7 +126,6 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             pipe.debug = True
         render_pkg = render(viewpoint_cam, gaussians, pipe, background)
         image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
-        gaussian_ids = render_pkg["gaussian_ids"]
         viewspace_point_tensor.retain_grad()
         # Add per view background over render alpha mask
         
@@ -205,7 +204,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             if iteration < opt.densify_until_iter:
                 # Keep track of max radii in image-space for pruning
                 gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
-                gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter, gaussian_ids=gaussian_ids)
+                gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter, meta=render_pkg["meta"])
 
                 if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None